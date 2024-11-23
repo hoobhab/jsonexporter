de986f5a179c26189ad4961de88a42f55111f16e
hoobhab <hoobhab3@gmail.com>
2024-11-23 13:10:32 -0800

    removed app password from testclient.py

--- testclient.py
+++ testclient.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5555")
     email_user = "hoobhab3@gmail.com"
-    email_pw = "mgar lbvm uksp ngem"
+    email_pw = "examplepassword"
     socket.send_json({"filename" : "packinglist.json", "username" : email_user, "password" : email_pw})
     # socket.send_json({"filename" : "packinglist.json", "username" : None, "password" : None})
     message = socket.recv()
